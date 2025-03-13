6aee0ee1ffce62d6fa849c884395bcc2b7b31081
buryakova.nn <buryakova.nn@parma.ru>
2025-03-13 22:02:16 +0500

    update test

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ SETUP
 npm ci
 npm run e2e:install
 
+set your github-login to env-variables "TEST_LOGIN" and "TEST_PASSWORD"
+
 RUN
 npm run e2e:smoke:ui
--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -7,7 +7,7 @@ const loginUser = process.env.TEST_LOGIN;
 const passUser = process.env.TEST_PASSWORD;
 
 test.beforeEach(async ({ page }) => {
-  await test.step("Authentication", async() => {
+  await test.step("Authentication", async () => {
     if (!loginUser || !passUser) {
       throw Error(`Provide "TEST_LOGIN" and "TEST_PASSWORD" inside .env`);
     }
@@ -22,20 +22,26 @@ test("Authentication. Viewing the main page. TEST CASE 0001", async ({ page }) =
 });
 
 
-test("Export. TEST CASE 0002", async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.repoLink.first().click();
-  const repoPage = new RepoPage(page);
-  await repoPage.btnCode.click();
-  const downloadPromise = page.waitForEvent("download");
-  await repoPage.btnDownload.click();
-  const download = await downloadPromise;
-  const fileName = download.suggestedFilename();
-  //await download.saveAs('e2e/download/'+fileName)
-  expect(fileName.includes(".zip"), "Имя скаченного файла не соответствует ожидаемому! (" + fileName + ")")
-  .toBeTruthy();
+test("Export. TEST CASE 0002",
+  { annotation: { type: "BUG", description: "<bug link>" } },
+  async ({ page }) => {
+    const mainPage = new MainPage(page);
+    await test.step(`Select first repo`, async () => {
+      await mainPage.repoLink.first().click();
+    });
+    const repoPage = new RepoPage(page);
+    await test.step(`Downloading ZIP`, async () => {
+      await repoPage.btnCode.click();
+      const downloadPromise = page.waitForEvent("download");
+      await repoPage.btnDownload.click();
+      const download = await downloadPromise;
+      const fileName = download.suggestedFilename();
+      //await download.saveAs('e2e/download/'+fileName)
+      expect(fileName.includes(".zip"), "File name is (" + fileName + ")")
+        .toBeTruthy();
+    });
 
-});
+  });
 
 test.afterEach(async ({ page }, testInfo) => {
   const screenshot = await page.screenshot();
